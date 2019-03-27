2120d1fe619aa60d2c5785dc6acf84d0899f0028
tungstencode <parteniealex@yahoo.com>
2019-03-27 22:31:13 +0200

    update

--- tag-parser-9gag.py
+++ tag-parser-9gag.py
@@ -1,58 +1,21 @@
-# from beautifulsoup4 import beautifulsoup4
-# import urllib2
-
-# response = urllib2.urlopen('http://en.wikipedia.org/wiki/Python_(programming_language)')
-# html = response.read()
- 
-# parsed_html = BeautifulSoup(html)
-# 
-# ------------------------------------------------------
-
 from bs4 import BeautifulSoup
-# import urllib2
 import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 url = "https://9gag.com"
-
 options=Options()
 options.headless=True
 browser=webdriver.Firefox(options=options)
 browser.get(url)
 # browser.setJavascriptEnabled(true)
 # browser.set_window_position(0, 0)
-for i in range(3):
+for i in range(40):
 	browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 	time.sleep(0.05)
-
 content = browser.page_source
-
 soup = BeautifulSoup(content,features="lxml")
-# print soup.prettify()
 pictures=soup.find_all('picture')
 print pictures
 print pictures.pop().find('img')['src']
-# print dir(pictures)
 browser.close()
 browser.quit()
-
-
-# ------------------------------------------------------
-
-# from bs4 import BeautifulSoup
-# import time
-# from selenium import webdriver
-
-# url = "https://www.basketball-reference.com/leagues/NBA_2017_standings.html"
-# browser = webdriver.Firefox()
-
-# browser.get(url)
-# time.sleep(3)
-# html = browser.page_source
-# soup = BeautifulSoup(html, "lxml")
-
-# print(len(soup.find_all("table")))
-# print(soup.find("table", {"id": "expanded_standings"}))
-
-# browser.close()
-# browser.quit()
--- tag-parser-tags.py
+++ tag-parser-tags.py
@@ -5,13 +5,11 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import sys
 url = "https://knowyourmeme.com/memes/trollface"
-
 if len(sys.argv)>1:
 	url=sys.argv[1]
 	print url
 else:
 	print 'maybe try a link'
-
 options=Options()
 options.headless=True
 browser=webdriver.Firefox(options=options)
@@ -20,7 +18,6 @@ content = browser.page_source
 soup = BeautifulSoup(content,features="lxml")
 browser.close()
 browser.quit()
-
 #find pictures
 print 'PICTURES BRO:'
 temp=soup.find('section',{"class":"bodycopy"}).find_all('center')
@@ -29,29 +26,11 @@ for center in temp:
 	pictures=center.find_all('img')
 	for img in pictures:
 		imgs.append(img)
-
 for img in imgs:
 	print img['data-src']
-
 
 #find tags
 print 'TAGS BRO:'
 tags=soup.find("dl", {"id": "entry_tags"}).find("dd").find_all('a')
 for element in tags:
 	print element.string
-
-
-
-
-
-#-----------------------------------
-
-
-# print pictures.pop().find('img')['src']
-# print dir(pictures)
-# print soup.prettify()
-# browser.setJavascriptEnabled(true)
-# browser.set_window_position(0, 0)
-# for i in range(3):
-# 	browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-# 	time.sleep(0.05)
