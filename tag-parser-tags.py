5e59ef48c4454dbbd4b9430eebbcf302886f012c
tungstencode <parteniealex@yahoo.com>
2019-03-30 12:31:26 +0200

    update

--- tag-parser-tags.py
+++ tag-parser-tags.py
@@ -18,23 +18,44 @@ browser=webdriver.Firefox(firefox_profile=firefox_profile,options=options)
 print 'pornim motoarele'
 # optimized
 #fac asta de 10 ori
-for k in range(5):
+for k in range(20):
 	# scot html din 10 falea
 	print 'ajung aici 1'
-	for i in range(5):
-		memesource.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		scrolltime=random.uniform(1,3)
-		print 'waiting to scroll for '+str(scrolltime)
-		time.sleep(scrolltime)
+	if k <1:
+		nrurls=0
+		for i in range(3):
+			memesource.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+			scrolltime=random.uniform(1,3)
+			print 'waiting to scroll for '+str(scrolltime)
+			time.sleep(scrolltime)
+	else:
+		for i in range(7):
+			memesource.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                        scrolltime=random.uniform(2.5,5)
+                        print 'waiting to scroll for '+str(scrolltime)
+                        time.sleep(scrolltime)
 	print 'ajung aici 2'
 	content = memesource.page_source
+	del content
 	memes = BeautifulSoup(content,features="lxml")
 	print 'ajung aici 3'
+	if k>0:
+		nrurls=len(urls)
+	else:
+		nrurls=0
 	urls=memes.find('tbody',{"class":"entry-grid-body infinite"}).find_all('a',{"class":"photo"})
+	del memes
+	sarituri=0
+	if nrurls<len(urls):
+		sarituri=nrurls
 	print 'getting html for the '+str(k)+' time'
-
+	print 'trecem prin '+str(len(urls))
+	print 'si sarim de '+str(sarituri)
 	# rulez alea 10 prin downloader
 	for urlB in urls:
+		if sarituri>1:
+			sarituri=sarituri-1
+			continue
 		url="https://knowyourmeme.com"+urlB['href']
 		print 'ajung aici 4'
 		dire=url.split("/")[-1]
@@ -44,11 +65,16 @@ for k in range(5):
 			   print 'we have '+dire+' already, next meme'
 			   continue
 		print 'ajung aici 5'
-		browser.get(url)
-		print 'ajung aici 6'
+		try:
+			browser.get(url)
+		except:
+			print 'ajung aici 6'
+			continue
 		content = browser.page_source
 		soup = BeautifulSoup(content,features="lxml")
+		del content
 		print 'getting data from '+url+', now filtering.'
+#		browser.close()
 		#find pictures
 		temp=soup.find('section',{"class":"bodycopy"}).find_all('center')
 		imgs=[]
@@ -58,9 +84,12 @@ for k in range(5):
 				imgs.append(img)
 		tocsv=[]
 		for img in imgs:
-			r = requests.get(img['data-src'], allow_redirects=True)
-			open(dire+"/"+img['data-src'].split("/")[-1], 'wb').write(r.content)
-			tocsv.append(img['data-src'].split("/")[-1])
+			try:
+				r = requests.get(img['data-src'], allow_redirects=True)
+				open(dire+"/"+img['data-src'].split("/")[-1], 'wb').write(r.content)
+				tocsv.append(img['data-src'].split("/")[-1])
+			except:
+				continue
 		#find tags
 		tagstocsv=[]
 		tags=soup.find("dl", {"id": "entry_tags"}).find("dd").find_all('a')
@@ -72,7 +101,7 @@ for k in range(5):
 		    writer.writerow(tagstocsv)
 		csvFile.close()
 		print dire+' meme was saved.'
-		waittime=random.uniform(1,3)
+		waittime=random.uniform(2,4)
 		print 'waiting to load url for '+str(waittime)
 		time.sleep(waittime)
 	#mai scot 10 dalea
@@ -80,24 +109,4 @@ browser.close()
 browser.quit()
 memesource.close()
 memesource.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print 'DONE'
